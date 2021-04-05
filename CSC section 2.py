60e1b744086fb668605fbc3895d41329ae91dcad
thegreatpanti <81993277+thegreatpanti@users.noreply.github.com>
2021-04-05 22:01:55 +0100

    Add files via upload

---
+++ CSC section 2.py
@@ -0,0 +1,29 @@
+print("Question one")
+print("ISAH HABEEB PANTI")
+print("COMPUTER SCIENCE")
+
+print("Question two")
+scarce="BOO!"
+print(scarce)
+
+print("Question three")
+first_variable=10
+second_variable=12
+third_variable=first_variable+second_variable
+print(third_variable)
+
+print("Question four")
+outcome=12/0.5
+print(outcome)
+
+print("Question five")
+result=4+6
+print(result)
+
+print("Question six")
+modu=8%3
+print(modu)
+
+print("Question seven")
+x=(2*4)*(4+2)
+print(x)
